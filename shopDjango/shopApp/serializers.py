964c4c4eb35ff66aab69895035141fd8a3ddac6c
TobiasComp <tuvia1234@gmail.com>
2020-06-21 18:12:58 +0300

    edited out the cart

--- shopDjango/shopApp/serializers.py
+++ shopDjango/shopApp/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Product, Order, OrderProductList, Cart
+from .models import Product, Order, OrderProductList
 
 
 class ProductSerializer(serializers.ModelSerializer):
@@ -17,7 +17,7 @@ class OrderProductListSerializer(serializers.ModelSerializer):
         model = OrderProductList
         fields = ['id', 'order', 'product', 'price']
 
-class CartSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Cart
-        fields = ['id', 'products']
+# class CartSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Cart
+#         fields = ['id', 'products']
